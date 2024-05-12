7b08d268dc5b1dae8f1711220d5ed55627e178f1
onlykyps <claudiu.pavel@hotmail.co.uk>
2024-05-12 12:55:00 +0300

    test de incarcare proiect

---
+++ proiect/aplicatie/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Transaction(models.Model):
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=90)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.country} --> {self.city}'
+
+
+class BudgetTransaction(models.Model):
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=90)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.country} --> {self.city}'
+
+
+
---
+++ proiect/aplicatie/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.views.generic import ListView, CreateView
+from django.urls import reverse
+from aplicatie.models import Transaction, BudgetTransaction
+
+
+# Create your views here.
+
+class BudgetTransactionView(ListView):
+    model = BudgetTransaction
+    template_name = 'aplicatie/transactions_index.html'
+
+
+class CreateBudgetTransactionView(CreateView):
+    model = BudgetTransaction
+    fields = ['city', 'country']
+    template_name = 'aplicatie/transactions_form.html'
+
+    def get_success_url(self):
+        return reverse('transactions:transactions_list')
